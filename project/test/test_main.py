718256a8acb2a7a06c6491cfb0cae34a67463c7b
MoltasA <103118351+MoltasA@users.noreply.github.com>
2022-05-18 13:38:46 +0200

    Update test_main.py

--- project/test/test_main.py
+++ project/test/test_main.py
@@ -25,18 +25,19 @@ obstacle_sensor = UltrasonicSensor(Port.S4)
 touch_sensor = TouchSensor(Port.S1)
 color_sensor = ColorSensor(Port.S3)
 robot = DriveBase(left_motor, right_motor, wheel_diameter= 47, axle_track= 128) # Initialize the drive base.
+v = 1
 
 ##Funktioner## #Måste testa#
 def height():
-    motorA.run_angle(200, 90, then = Stop.HOLD, wait = True)
-    #while touch_sensor.pressed() == False:
-    robot.drive(-75,0)
+    motorA.run_angle(v*-500, 90, then = Stop.HOLD, wait = True)
+    while touch_sensor.pressed() == False:
+        robot.drive(v*-50,0)
     if touch_sensor.pressed() == True:
         ev3.speaker.say('Picking up an item')
-        motorA.run_angle(500, 180, then = Stop.HOLD, wait = True)
-        robot.drive(50,0)
-    
-        
+        motorA.run_angle(v*500, 180, then = Stop.HOLD, wait = True)
+        robot.drive(v*50,0)
+        robot.stop()
+           
     elif touch_sensor.pressed() == False:
         print("Det upplockade obejektet har tappats")
 
@@ -50,7 +51,7 @@ def line_follow():
     WHITE = 85
     threshold = (BLACK + WHITE) / 2
 
-    DRIVE_SPEED = 50
+    DRIVE_SPEED = v*50
     PROPORTIONAL_GAIN = 1.2
 
     while True:
@@ -62,14 +63,14 @@ def line_follow():
 
         wait(10)
 
-        if touch_sensor.pressed() == True:
-            pickup(touch_sensor, color_sensor)
+        #if touch_sensor.pressed() == True:
+         #   pickup(touch_sensor, color_sensor)
 
 #Undvika Kollision
         while obstacle_sensor.distance() < 170:
             wait(10)
             print(obstacle_sensor.distance())
-            robot.straight(-150)
+            robot.straight(v*-150)
             robot.turn(120)
             print('Undviker kollision')
         
@@ -92,9 +93,9 @@ def area(color_sensor, color):
 #Plocka upp## #Klar#
 def pickup(touch_sensor, color_sensor):
     if touch_sensor.pressed() == True:
-        motorA.run(200)
-        robot.straight(-50)
-        motorA.run(-200)
+        motorA.run(v*200)
+        robot.straight(v*-50)
+        motorA.run(v*-200)
         robot.turn(360)
         print("Roboten har tagit upp något")
         
@@ -108,12 +109,12 @@ def collision():
     obstacle_sensor = UltrasonicSensor(Port.S4)
 
     while True:
-        robot.drive(-50, 0)
+        robot.drive(v*-50, 0)
         print(obstacle_sensor.distance())
         while obstacle_sensor.distance() > 170:
             wait(10)
             print(obstacle_sensor.distance())
-            robot.straight(-150)
+            robot.straight(v*-150)
             robot.turn(120)
 
 ##Follow line 2##
@@ -162,29 +163,47 @@ def just_pickup(touch_sensor):
     #motorA.run_angle(-500, 90, then = Stop.HOLD, wait = True)
     #while touch_sensor.pressed() == False:
     while True:
-        robot.drive(-75,0)
+        robot.drive(v*-75,0)
         if touch_sensor.pressed() == True:
             ev3.speaker.say('Picked up an item')
-            motorA.run_angle(500, 180, then = Stop.HOLD, wait = True)
-            robot.drive(50,0)
+            motorA.run_angle(v*500, 180, then = Stop.HOLD, wait = True)
+            robot.drive(v*50,0)
             break
-    
-        if touch_sensor.pressed() == False:
-            print("Det upplockade obejektet har tappats")
             
+
 
 def collision():
     obstacle_sensor = UltrasonicSensor(Port.S4)
 
     while True:
-        robot.drive(-100, 0)
+        robot.drive(v*-100, 0)
         print(obstacle_sensor.distance())
         while obstacle_sensor.distance() < 200:
-            ev3.speaker.say('Avoided an collision')
             wait(10)
-            print(obstacle_sensor.distance())
-            robot.straight(50)
+            robot.straight(v*50)
             robot.turn(120)
+            ev3.speaker.say('Avoided an collision')
+
+
+def just_line_follow():
+    # Initialize the color sensor.
+    color_sensor = ColorSensor(Port.S3)
+
+    BLACK = 9
+    WHITE = 85
+    threshold = (BLACK + WHITE) / 2
+
+    DRIVE_SPEED = v*-50
+    PROPORTIONAL_GAIN = 1.2
+
+    while True:
+        deviation = color_sensor.reflection() - threshold #threshold
+
+        turn_rate = PROPORTIONAL_GAIN * deviation
+
+        robot.drive(DRIVE_SPEED, turn_rate)
+
+        wait(10)
 
 
 
